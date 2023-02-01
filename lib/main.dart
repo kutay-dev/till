9211f1e35dd9886f1fc600ffc8eaf172fe4a2d1d
evolade <kutaycereci89@gmail.com>
2023-02-01 21:30:20 +0300

    add ignore comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable, use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
